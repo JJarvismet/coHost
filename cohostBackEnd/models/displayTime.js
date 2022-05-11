9319b1a5dac5d34c8bde4bf2bab0d3f46c8b7cd4
Jeremy Jarvis <jarvismet@gmail.com>
2022-05-10 22:44:00 -0500

    fixed time displays

--- cohostBackEnd/models/displayTime.js
+++ cohostBackEnd/models/displayTime.js
@@ -1,10 +1,16 @@
 const displayTime = (time) =>{
     if(time){
         if(time.charAt(0) === '0'){
+            if(parseInt(time.substring(0,2))===00){
+                return `${parseInt(time.substring(0,2))+12}${time.substring(2,4)}am`
+            }
             return `${time.substring(1,5)}am`;
         }
-        if(parseInt(time.substring(0,2))>=13){
-            return `${parseInt(time.substring(0,2))-12}${time.substring(2,5)}pm`
+        if(parseInt(time.substring(0,2))>=12){
+            if(parseInt(time.substring(0,2))===12){
+                return `${time}pm`
+            }
+            return `${parseInt(time.substring(0,2))-12}${time.substring(2,4)}pm`
         }else{
             return `${time}am`
         }
